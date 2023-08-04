fe6c9f02af4e22f3977dafc2d3a2cb7dc2e6956f
Angel09122002 <gabrielquintero1221@gmail.com>
2023-08-04 16:56:12 -0400

    Navbar-css responsive added(fix the contact button later)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import Navbar from "./components/navbar/Navbar"
 import React from "react";
+
+import Navbar from "./components/navbar/Navbar";
+import Home from "./components/home/Home";
 
 function App() {
   return (
     <React.Fragment>
-      <Navbar/>
+          <Navbar /> 
     </React.Fragment>
   );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,80 +1,89 @@
-import React, { useState } from "react";
-import "../../styles/navbar.css"
-import { FaBars, FaTimes } from "react-icons/fa";
-import { Link } from "react-scroll";
+import React, { useState, useEffect } from "react";
+import "../../styles/navbar.css";
 import logonavbar from "../../assets/logonavbar.png";
+import { Link } from "react-scroll";
+import { Button } from "../button/Button";
 
 const NavBar = () => {
-  const [nav, setNav] = useState(false);
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
-  const links = [
-    {
-      id: 1,
-      link: "Home",
-    },
-    {
-      id: 2,
-      link: "About us",
-    },
-    {
-      id: 3,
-      link: "Services",
-    },
-    {
-      id: 4,
-      link: "Contact",
-    },
-  ];
+  const handleClick = () => {
+    setClick(!click);
+  };
+
+  const closeMobilemenu = () => {
+    setClick(false);
+  };
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+    window.addEventListener("resize", showButton);
+    return () => {
+      window.removeEventListener("resize", showButton);
+    };
+  }, []);
 
   return (
-    <nav className="top-bar">
-      <a id="img" href="/">
-        <img  src={logonavbar} alt="Logo-navbar" width={150} style={{ width: "150px", height: "auto" }} />
-      </a>
-      <a href="/" className="title-navbar">HappyTailsPetsSpa</a>
-
-      <div>
-        <ul className="navbar">
-          {links.map(({ id, link }) => (
-            <li
-              key={id}
-              className="burgericon1"
-            >
-              <Link to={link} smooth duration={500}>
-                {link}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-
-      <div
-        onClick={() => setNav(!nav)}
-        className="onlclick"
-      >
-        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
-      </div>
-
-      {nav && (
-        <div className="burger">
-          {links.map(({ id, link }) => (
-            <li
-              key={id}
-              className="openburger"
-            >
-              <Link
-                onClick={() => setNav(!nav)}
-                to={link}
-                smooth
-                duration={400}
-              >
-                {link}
-              </Link>
-            </li>
-          ))}
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo" onClick={closeMobilemenu}>
+            Happy<i className="fas fa-paw"></i>
+          </Link>
         </div>
-      )}
-    </nav>
+        <div className="menu-icon" onClick={handleClick}>
+          <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
+        </div>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <Link to="/" className="nav-links" onClick={closeMobilemenu}>
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/about us"
+              className="nav-links"
+              onClick={closeMobilemenu}
+            >
+              About
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/services"
+              className="nav-links"
+              onClick={closeMobilemenu}
+            >
+              Services
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/contact"
+              className="nav-links-mobile"
+              onClick={closeMobilemenu}
+            >
+              Contact us
+            </Link>
+          </li>
+        </ul>
+        {button && (
+          <Button buttonStyle="btn--outline">
+            Contact us
+          </Button>
+        )}
+      </nav>
+    </>
   );
 };
 
