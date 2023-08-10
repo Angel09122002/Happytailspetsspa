2f5f9d5f03d9193ddd8735c0d3265a609764147f
Angel09122002 <gabrielquintero1221@gmail.com>
2023-08-10 19:38:22 -0400

    HomeComponet added

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,24 +1,31 @@
 import React from 'react'
 import "../../styles/home.css"
 import bathhome from "../../assets/bathhome.jpg"
+import scissors from "../../assets/scissors.png"
 import { Button } from '../button/Button'
+
+
+
 const Home = () => {
   return (
-    <div className="home-container">
-      <img src={bathhome} alt="bathcome" />
-
-      <div className="home-wrapper">
-        <div className="home-container">
-          <div className="Text-homeContainer">
-            <p>
-              <strong>
-              Enhance your furry companion's appearance with comfort and care!
-              </strong>
-              
-            </p>
-          </div>
-        </div>
-        <Button buttonStyle="btn--outline">Contact Your Groomer</Button>
+    <div className="hero-container">
+      <h1>Enhance your furry companion's apperance </h1>
+      <p>with comfort and care!</p>
+      <div className="hero-btns">
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          Contact Your Grommer
+        </Button>
+        <Button
+          className="btns"
+          buttonStyle="btn--primary"
+          buttonSize="btn--large"
+        >
+          See our magic  <i class="fa-brands fa-instagram"></i>
+        </Button>
       </div>
     </div>
   );
