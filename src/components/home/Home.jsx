a58c989f68b65926cb3ab2322b6afc47a111de8d
Angel0912200 <gabrielquintero1221@gmail.com>
2023-08-10 18:22:13 -0400

    /

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,22 +4,24 @@ import bathhome from "../../assets/bathhome.jpg"
 import { Button } from '../button/Button'
 const Home = () => {
   return (
-    <div className='home-container'>
-       <img 
-          src={bathhome}
-          alt="bathcome"
-        />
-        
-        <div className='home-wrapper'>
-          <div className='home-container'>
-              <div className='Text-homeContainer'>
-              <p>Necesitas un asljkdak</p>
-              </div>
+    <div className="home-container">
+      <img src={bathhome} alt="bathcome" />
+
+      <div className="home-wrapper">
+        <div className="home-container">
+          <div className="Text-homeContainer">
+            <p>
+              <strong>
+              Enhance your furry companion's appearance with comfort and care!
+              </strong>
+              
+            </p>
           </div>
-          <Button  buttonStyle="btn--outline">Contact Your Groomer</Button>
         </div>
+        <Button buttonStyle="btn--outline">Contact Your Groomer</Button>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Home
