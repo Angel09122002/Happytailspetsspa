2e761b755105342ce3944fa1471d27dd97958d78
Angel09122002 <gabrielquintero1221@gmail.com>
2023-08-24 23:56:02 -0400

    Nav-Responsive fixed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -36,59 +36,63 @@ const NavBar = () => {
   return (
     <>
       <nav className="navbar">
-        <img
-          src={logonavbar}
-          alt="Logo-navbar"
-          style={{ width: "150px", height: "auto" }}
-        />
+        <div className="Logo">
+          <img
+            src={logonavbar}
+            alt="Logo-navbar"
+            style={{ width: "150px", height: "auto" }}
+          />
 
-        <div className="navbar-container">
-          <Link to="/" className="navbar-logo" onClick={closeMobilemenu}>
-            <p>Happy Tails Pets Spa</p>
-          </Link>
+          <div className="navbar-container">
+            <Link to="/" className="navbar-logo" onClick={closeMobilemenu}>
+              <p>Happy Tails Pets Spa</p>
+            </Link>
+          </div>
         </div>
+
         <div className="menu-icon" onClick={handleClick}>
           <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
         </div>
-        <ul className={click ? "nav-menu active" : "nav-menu"}>
+        <div className={click ? "nav-menu active" : "nav-menu"}>
+          <ul className="nav-ul">
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobilemenu}>
+                Home
+              </Link>
+            </li>
 
-          
-          <li className="nav-item">
-            <Link to="/" className="nav-links" onClick={closeMobilemenu}>
-              Home
-            </Link>
-          </li>
-
-
-          <li className="nav-item">
-            <Link
-              to="/about us"
-              className="nav-links"
-              onClick={closeMobilemenu}
-            >
-              About
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              to="/services"
-              className="nav-links"
-              onClick={closeMobilemenu}
-            >
-              Services
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              to="/contact"
-              className="nav-links-mobile"
-              onClick={closeMobilemenu}
-            >
-              Contact us
-            </Link>
-          </li>
-        </ul>
-        {button && <Button buttonStyle="btn--outline">Contact us</Button>}
+            <li className="nav-item">
+              <Link
+                to="/about us"
+                className="nav-links"
+                onClick={closeMobilemenu}
+              >
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/services"
+                className="nav-links"
+                onClick={closeMobilemenu}
+              >
+                Services
+              </Link>
+            </li>
+            <li className={click ? "nav-item" : "noMobileContactUs"}>
+              <Link
+                to="/contact"
+                className="nav-links-mobile"
+                onClick={closeMobilemenu}
+              >
+                Contact us
+              </Link>
+            </li>
+            <div className="container_button">
+              <button className="BtnContact">Contact us</button>
+            </div>
+          </ul>
+        </div>
       </nav>
     </>
   );
