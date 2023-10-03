5669bd9ec776e0e094bb913b959394b75f235af0
Angel09122002 <gabrielquintero1221@gmail.com>
2023-10-03 10:50:00 -0400

    error react-router fix

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+
 import "../../styles/navbar.css";
 import logonavbar from "../../assets/logonavbar.png";
 import "../../styles/button.css";
